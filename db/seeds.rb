96490f1590a7962eea7d542ef7a27d4cf38b901f
Wilson.ST <wilsonthehungryman@gmail.com>
2017-03-23 22:04:24 +0000

    Added seed data for: Country, Province, and Address.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,28 @@
 AdminUser.create!(email: 'wilsonthehungryman@gmail.com',
                   password: 'andhebelike,passwordISpassword',
                   password_confirmation: 'andhebelike,passwordISpassword')
+canada = Country.create(name: 'Canada', abbreviation: 'CAN')
+
+mb = Province.create(name: 'Manitoba', abbreviation: 'MB', country: canada)
+
+Province.create(name: 'Alberta', abbreviation: 'AB', country: canada)
+Province.create(name: 'Ontario', abbreviation: 'ON', country: canada)
+Province.create(name: 'British Columbia', abbreviation: 'BC', country: canada)
+
+Address.create(city: 'Winnipeg',
+               street: 'Harvard Ave.',
+               street_number: '246',
+               postal_code: 'R3M 0K7',
+               province: mb)
+
+Address.create(city: 'Winnipeg',
+               street: 'Yale Ave.',
+               street_number: '246',
+               postal_code: 't3a 0K7',
+               province: mb)
+
+Address.create(city: 'Brandon',
+               street: 'Corydon',
+               street_number: '999',
+               postal_code: 'a1a1a1',
+               province: mb)
