0203da9c1f973fabbdc0a68670fe26b75b6fddea
Wilson.ST <wilsonthehungryman@gmail.com>
2017-04-04 16:36:22 -0500

    Add Redcarpet to the site content model. When creating a record, the content field will be converted to html using Redcarpet.

--- app/models/site_content.rb
+++ app/models/site_content.rb
@@ -1,2 +1,11 @@
+#
 class SiteContent < ApplicationRecord
+  validates :content, :page, :index, presence: true
+  validates :index, uniqueness: { scope: :page }
+  before_create do
+    renderer = Redcarpet::Render::HTML.new(filter_html: false, no_image: false,
+                                           no_links: false)
+    markdown = Redcarpet::Markdown.new(renderer)
+    self.content = markdown.render(content)
+  end
 end
